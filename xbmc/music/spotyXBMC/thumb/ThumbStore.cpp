7e0aef894f497b6a55dccda830cd8923c4b41e10
Valentin Grouès <neoflexx@gmail.com>
2012-05-06 00:35:23 +0200

    merged with upstream

--- xbmc/music/spotyXBMC/thumb/ThumbStore.cpp
+++ xbmc/music/spotyXBMC/thumb/ThumbStore.cpp
@@ -27,8 +27,8 @@
 #include "../Utils.h"
 #include <string.h>
 #include "URL.h"
-#include "../../../filesystem/FileCurl.h"
-#include "tinyXML/tinyxml.h"
+#include "../../../filesystem/CurlFile.h"
+#include "../utils/XBMCTinyXML.h"
 #include <fstream>
 #include "../../../filesystem/File.h"
 
@@ -144,7 +144,7 @@ namespace addon_music_spotify {
 		if (it == m_fanarts.end()) {
 			//we dont have it so we need to to a search for it
 
-			CFileCurl http;
+			CCurlFile http;
 			CStdString artistString = artistName;
 			artistString.Replace(' ', '+');
 			CStdString urlString;
